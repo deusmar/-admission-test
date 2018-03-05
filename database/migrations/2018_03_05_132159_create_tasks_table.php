8aa1a3b787fdbbcf2db88fcc240b7ea0943d36c6
Deusmar Junior <deusmar.junior@terceirzado.mds.gov.br>
2018-03-05 12:09:31 -0300

    Fix user table. Attending to User Story 8 Acceptance Criterias 1 & 2

--- database/migrations/2018_03_05_132159_create_tasks_table.php
+++ database/migrations/2018_03_05_132159_create_tasks_table.php
@@ -15,13 +15,13 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('owner_id');
+            $table->unsignedInteger('owner_id')->nullable(); //US8_AC1_AC2
             $table->unsignedInteger('lane_id')->nullable()->default(1); // US7_AC5
             $table->unsignedInteger('order_id');
             $table->string('name');
             $table->timestamps();
 
-            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('set null');
             $table->foreign('lane_id')->references('id')->on('lanes')->onDelete('set default');
         });
     }
