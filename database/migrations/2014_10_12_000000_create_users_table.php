a89982026171a1b67db187b288ef11dbd9e47b8f
Deusmar Junior <deusmar.junior@terceirzado.mds.gov.br>
2018-03-05 12:01:59 -0300

    Email verification settings on User Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('verified')->default(false); //US5_AC1
+            $table->string('verify_token');
             $table->rememberToken();
             $table->timestamps();
         });
